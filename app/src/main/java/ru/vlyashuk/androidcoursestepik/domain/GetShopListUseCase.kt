b0ecda291031ca77519a2a68ae0654d851186ade
Sr_Lyashuk <sesila94@mail.ru>
2025-02-05 17:15:57 +0300

    Created Domain layer Shopping List

---
+++ app/src/main/java/ru/vlyashuk/androidcoursestepik/domain/GetShopListUseCase.kt
@@ -0,0 +1,8 @@
+package ru.vlyashuk.androidcoursestepik.domain
+
+class GetShopListUseCase(private val shopListRepository: ShopListRepository) {
+
+    fun getShopList(): List<ShopItem> {
+        return shopListRepository.getShopList()
+    }
+}
