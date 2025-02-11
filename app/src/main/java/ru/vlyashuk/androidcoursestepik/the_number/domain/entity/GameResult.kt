186ef120324cdfa4f84b854e3c69b81c645a8bbe
Sr_Lyashuk <sesila94@mail.ru>
2025-02-11 15:33:15 +0300

    Setting navigation using FragmentManager

--- app/src/main/java/ru/vlyashuk/androidcoursestepik/the_number/domain/entity/GameResult.kt
+++ app/src/main/java/ru/vlyashuk/androidcoursestepik/the_number/domain/entity/GameResult.kt
@@ -1,8 +1,10 @@
 package ru.vlyashuk.androidcoursestepik.the_number.domain.entity
 
+import java.io.Serializable
+
 data class GameResult(
     val winner: Boolean,
     val countOfRightAnswers: Int,
     val countOfQuestions: Int,
     val gameSettings: GameSettings
-)
+) :Serializable
