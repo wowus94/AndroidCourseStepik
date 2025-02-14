3e019c5f025d5412c4e8182ec8613cd302e0ef8c
Sr_Lyashuk <sesila94@mail.ru>
2025-02-14 09:40:39 +0300

    Add Crypto App package

---
+++ app/src/main/java/ru/vlyashuk/androidcoursestepik/crypto_app/presentation/MainCryptoAppActivity.kt
@@ -0,0 +1,12 @@
+package ru.vlyashuk.androidcoursestepik.crypto_app.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.vlyashuk.androidcoursestepik.R
+
+class MainCryptoAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_crypto_app)
+    }
+}
