110ad130e049be7540fb53e8fddfcd29a178413f
Владимир Ляшук <89472439+wowus94@users.noreply.github.com>
2025-02-16 22:34:27 +0300

    Component Builder. Transfer parameters to the module

--- app/src/main/java/ru/vlyashuk/androidcoursestepik/dagger_app/example_two/data/network/ExampleApiService.kt
+++ app/src/main/java/ru/vlyashuk/androidcoursestepik/dagger_app/example_two/data/network/ExampleApiService.kt
@@ -1,12 +1,16 @@
 package ru.vlyashuk.androidcoursestepik.dagger_app.example_two.data.network
 
+import android.content.Context
 import android.util.Log
+import ru.vlyashuk.androidcoursestepik.R
 import javax.inject.Inject
 
-class ExampleApiService @Inject constructor() {
+class ExampleApiService @Inject constructor(
+    private val context: Context
+) {
 
     fun method() {
-        Log.i(LOG_TAG, "ExampleApiService")
+        Log.i(LOG_TAG, "ExampleApiService ${context.getString(R.string.dagger2)}")
     }
 
     companion object {
